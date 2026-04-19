72c4a57ab400c0d9bfe4c39de2760bb92e921802
PSerratLloveras <114682421+PolSerrat@users.noreply.github.com>
2026-04-20 00:39:26 +0200

    High Score UI 2

--- Assets/Scripts/Managers/GameStateManager.cs
+++ Assets/Scripts/Managers/GameStateManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class GameStateManager : MonoBehaviour
 {
     public static GameStateManager Instance; // 1
@@ -14,9 +14,14 @@ public class GameStateManager : MonoBehaviour
     public int sheepDroppedBeforeGameOver; // 4
     public SheepSpawner sheepSpawner; // 5
 
+    public int highScore;
+    public Text highScoreText;
+
     void Awake()
     {
         Instance = this;
+        // If there is no saved score, it defaults to 0
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 
     // Update is called once per frame
@@ -39,6 +44,16 @@ public class GameStateManager : MonoBehaviour
         sheepSpawner.canSpawn = false; // 1
         sheepSpawner.DestroyAllSheep(); // 2
         UIManager.Instance.ShowGameOverWindow();
+
+        int currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
+        if (sheepSaved > highScore)
+        {
+            highScore = sheepSaved;
+            currentHighScore = highScore;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save(); // Locks it in!
+        }
+        highScoreText.text = "HS: " + currentHighScore.ToString(); // 3    
 
     }
     public void DroppedSheep()
